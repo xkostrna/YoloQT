12795fb3a5de90c071d06e7434ad163c7ccfdd70
kostr <kostrnatomas@gmail.com>
2024-03-22 17:42:30 +0100

    style: add code headers

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ import sys
 from gui.mainwindow import AppMainWindow
 from PySide6.QtWidgets import QApplication
 
+__authors__ = ["Bc. Tomas Kostrna", "Ing. Pavol Marak, PhD."]
+__license__ = "MIT"
+__faculty__ = "Faculty of Electrical Engineering and Information Technology of STU in Bratislava"
+__institute__ = "Department of Informatics and Mathematics"
+
 if __name__ == "__main__":
     app = QApplication()
     widget = AppMainWindow()
